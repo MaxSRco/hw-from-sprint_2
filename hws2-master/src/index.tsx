f97697d002e232302240813eccbdb5b6f67303e3
SergeyMaximov <mrknitter7@gmail.com>
2025-07-21 21:31:45 +0300

    hw06:init

--- hws2-master/src/index.tsx
+++ hws2-master/src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
-        <Provider store={store}>
-            <App/>
-        </Provider>
+            <Provider store={store}>
+                <App/>
+            </Provider>
     </React.StrictMode>
 )
 
