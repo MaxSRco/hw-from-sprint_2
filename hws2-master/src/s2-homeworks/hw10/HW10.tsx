7b720a7ea43bb403e96227cfcdde8eac60e32283
SergeyMaximov <mrknitter7@gmail.com>
2025-11-10 16:55:19 +0300

    hw10

--- hws2-master/src/s2-homeworks/hw10/HW10.tsx
+++ hws2-master/src/s2-homeworks/hw10/HW10.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
@@ -14,35 +14,41 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
-
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
-    }
-
-    return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
-
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
-            </div>
-        </div>
-    )
+  // useSelector, useDispatch // пишет студент
+  // const isLoading = false
+  const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
+  const dispatch = useDispatch()
+  
+  const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+    // dispatch
+    dispatch(loadingAC(true))
+    // setTimeout
+    setTimeout(()=>{
+      dispatch(loadingAC(false))
+    }, 1500)
+  }
+  
+  return (
+    <div id={'hw10'}>
+      <div className={s2.hwTitle}>Homework #10</div>
+      
+      <div className={s2.hw}>
+        {isLoading ? (
+          <div id={'hw10-loading'}>
+            <Loader/>
+          </div>
+        ) : (
+          <SuperButton
+            style={{width: 'fit-content', height: 'fit-content', padding: '5px 24px'}}
+            id={'hw10-button-start-loading'}
+            onClick={setLoading}
+          >
+            Set loading...
+          </SuperButton>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default HW10
